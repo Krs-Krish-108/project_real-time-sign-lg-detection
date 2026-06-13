e079184640d2f32d82bdd4b4488c3cd2db740660
jayustudent <jppandey849@gmail.com>
2026-06-13 14:48:00 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Real_time_sign_language_translation/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
